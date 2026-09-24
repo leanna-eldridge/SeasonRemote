85495a664905a8a341122330ef9dffa02721388d
leldridg <leldridg@trinity.edu>
2026-09-23 22:03:38 -0700

    Update modinfo and make it so season settings persist when leaving and re-entering the world

--- SeasonRemote/SeasonRemoteModSystem.cs
+++ SeasonRemote/SeasonRemoteModSystem.cs
@@ -3,130 +3,162 @@ using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 
-[assembly: ModInfo("SeasonRemote", "seasonRemote",
-                    Authors = new string[] { "Leah" },
-                    Description = "This mod enables you to pause the changing of the seasons at will.",
-                    Version = "1.0.0")]
+[assembly: ModInfo(
+    "Season Remote",
+    "seasonremote",
+    Authors = new[] { "ihaskittykat" },
+    Description = "This mod enables you to pause the changing of the seasons at will.",
+    Version = "1.0.1"
+)]
 
-namespace SeasonRemote
+namespace SeasonRemote;
+
+[ProtoContract]
+public class SeasonRemotePacket
 {
-    [ProtoContract]
-    public class SeasonRemotePacket
-    {
-        [ProtoMember(1)]
-        public bool Paused;
+    [ProtoMember(1)]
+    public bool Paused;
 
-        [ProtoMember(2)]
-        public float SeasonRel;
+    [ProtoMember(2)]
+    public float? SeasonRel;
+}
+
+public class SeasonRemoteModSystem : ModSystem
+{
+    private const string PausedKey = "seasonremote:paused";
+    private const string SeasonRelKey = "seasonremote:seasonrel";
+
+    private bool isPaused;
+    private float? pausedSeasonRel;
+
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+
+        api.Network
+            .RegisterChannel("seasonRemote")
+            .RegisterMessageType<SeasonRemotePacket>();
     }
 
-    public class SeasonRemoteModSystem : ModSystem
+    public override void StartServerSide(ICoreServerAPI api)
     {
-        bool isPaused = false;
+        base.StartServerSide(api);
 
-        public override void Start(ICoreAPI api)
+        api.Event.SaveGameLoaded += () =>
         {
-            base.Start(api);
+            isPaused = api.World.Config.GetBool(PausedKey, false);
+            pausedSeasonRel = api.World.Config.GetFloat(SeasonRelKey, 0f);
 
+            if (isPaused)
+            {
+                api.World.Calendar.SetSeasonOverride(pausedSeasonRel);
+            }
+        };
+
+        api.Event.PlayerJoin += (player) =>
+        {
+            SendSeasonState([player]);
+        };
+
+        IChatCommand seasonCommandBase = api.ChatCommands.GetOrCreate("season")
+            .WithDescription("See current season information or manipulate the change of the seasons.")
+            .RequiresPrivilege(Privilege.chat)
+            .RequiresPlayer();
+
+        // Pause the season if not currently paused
+        seasonCommandBase.BeginSubCommand("pause")
+            .WithDescription("Pause the change of the seasons. The season will continue to be the current one.")
+            .HandleWith((args) =>
+            {
+                var playerPos = args.Caller.Pos.AsBlockPos;
+                var seasonName = api.World.Calendar.GetSeason(playerPos);
+
+                if (isPaused)
+                {
+                    return TextCommandResult.Success($"The seasons are already on pause. \n" +
+                        $"The current season is {seasonName}");
+                }
+
+                SetSeasonState(paused: true, seasonRel: (float)api.World.Calendar.YearRel);
+                SendSeasonState(api.Server.Players);
+                SaveSeasonState();
+
+                return TextCommandResult.Success($"The seasons have been paused. It will remain {seasonName}.");
+            })
+            .EndSubCommand();
+
+        // Resume the season if currently paused
+        seasonCommandBase.BeginSubCommand("resume")
+            .WithDescription("Resume the change of the seasons. The season will become the one typically associated with the current date.")
+            .HandleWith((args) =>
+            {
+                if (!isPaused)
+                {
+                    return TextCommandResult.Success("The seasons are already changing!");
+                }
+
+                SetSeasonState(paused: false, seasonRel: null);
+                SendSeasonState(api.Server.Players);
+                SaveSeasonState();
+
+                return TextCommandResult.Success("The seasons have resumed changing!");
+            })
+            .EndSubCommand();
+
+        // Show current season and whether it is paused
+        seasonCommandBase.BeginSubCommand("info")
+            .WithDescription("Get information about the current season and whether the seasons are changing or on pause.")
+            .HandleWith((args) =>
+            {
+                var message = string.Empty;
+                var playerPos = args.Caller.Pos.AsBlockPos;
+                var seasonName = api.World.Calendar.GetSeason(playerPos);
+
+                if (isPaused)
+                {
+                    message = $"The seasons are on pause. \n" +
+                    $"The current season is {seasonName}";
+                }
+                else
+                {
+                    message = $"The seasons are changing! \n" +
+                    $"The current season is {seasonName}";
+                }
+
+                return TextCommandResult.Success(message);
+            })
+            .EndSubCommand();
+
+        void SetSeasonState(bool paused, float? seasonRel)
+        {
+            isPaused = paused;
+            pausedSeasonRel = seasonRel;
+            api.World.Calendar.SetSeasonOverride(pausedSeasonRel);
+        }
+
+        void SendSeasonState(IServerPlayer[] players)
+        {
             api.Network
-                .RegisterChannel("seasonRemote")
-                .RegisterMessageType<SeasonRemotePacket>();
+                    .GetChannel("seasonRemote")
+                    .SendPacket(new SeasonRemotePacket { Paused = isPaused, SeasonRel = pausedSeasonRel }, players);
         }
 
-        public override void StartServerSide(ICoreServerAPI api)
+        void SaveSeasonState()
         {
-            base.StartServerSide(api);
-
-            IChatCommand seasonCommandBase = api.ChatCommands.GetOrCreate("season")
-                .WithDescription("See current season information or manipulate the change of the seasons.")
-                .RequiresPrivilege(Privilege.chat)
-                .RequiresPlayer();
-
-            // Pause the season if not currently paused
-            seasonCommandBase.BeginSubCommand("pause")
-                .WithDescription("Pause the change of the seasons. The season will continue to be the current one.")
-                .HandleWith((args) =>
-                {
-                    var seasonRel = (float)api.World.Calendar.YearRel;
-
-                    var playerPos = args.Caller.Pos.AsBlockPos;
-                    var seasonName = api.World.Calendar.GetSeason(playerPos);
-
-                    if (isPaused)
-                    {
-                        return TextCommandResult.Success($"The seasons are already on pause. \n" +
-                            $"The current season is {seasonName}");
-                    }
-
-                    api.World.Calendar.SetSeasonOverride(seasonRel);
-                    isPaused = true;
-
-                    api.Network
-                        .GetChannel("seasonRemote")
-                        .SendPacket(new SeasonRemotePacket { Paused = isPaused, SeasonRel = seasonRel }, api.Server.Players);
-
-                    return TextCommandResult.Success($"The seasons have been paused. It will remain {seasonName}.");
-                })
-                .EndSubCommand();
-
-            // Resume the season if currently paused
-            seasonCommandBase.BeginSubCommand("resume")
-                .WithDescription("Resume the change of the seasons. The season will become the one typically associated with the current date.")
-                .HandleWith((args) =>
-                {
-                    if (!isPaused)
-                    {
-                        return TextCommandResult.Success("The seasons are already changing!");
-                    }
-
-                    api.World.Calendar.SetSeasonOverride(null);
-                    isPaused = false;
-
-                    api.Network
-                        .GetChannel("seasonRemote")
-                        .SendPacket(new SeasonRemotePacket { Paused = isPaused }, api.Server.Players);
-
-                    return TextCommandResult.Success("The seasons have resumed changing!");
-                })
-                .EndSubCommand();
-
-            // Show current season and whether it is paused
-            seasonCommandBase.BeginSubCommand("info")
-                .WithDescription("Get information about the current season and whether the seasons are changing or on pause.")
-                .HandleWith((args) =>
-                {
-                    var seasonRel = (float)api.World.Calendar.YearRel;
-                    var message = string.Empty;
-
-                    var playerPos = args.Caller.Pos.AsBlockPos;
-                    var seasonName = api.World.Calendar.GetSeason(playerPos);
-
-                    if (isPaused)
-                    {
-                        message = $"The seasons are on pause. \n" +
-                        $"The current season is {seasonName}";
-                    }
-                    else
-                    {
-                        message = $"The seasons are changing! \n" +
-                        $"The current season is {seasonName}";
-                    }
-
-                    return TextCommandResult.Success(message);
-                })
-                .EndSubCommand();
+            api.World.Config.SetBool(PausedKey, isPaused);
+            api.World.Config.SetFloat(SeasonRelKey, pausedSeasonRel ?? 0f);
         }
+    }
 
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-            base.StartClientSide(api);
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
 
-            api.Network
-                .GetChannel("seasonRemote")
-                .SetMessageHandler<SeasonRemotePacket>(packet =>
-                {
-                    api.World.Calendar.SetSeasonOverride(packet.Paused ? packet.SeasonRel : null);
-                });
-        }
+        api.Network
+            .GetChannel("seasonRemote")
+            .SetMessageHandler<SeasonRemotePacket>(packet =>
+            {
+                api.World.Calendar.SetSeasonOverride(packet.Paused ? packet.SeasonRel : null);
+            });
     }
 }
